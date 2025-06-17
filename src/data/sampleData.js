ad8ebec4c2c6bf65031416770d41ad40179ef08b
krojoh <chrojoh12@gmail.com>
2025-06-17 02:02:39 -0600

    Create sampleData.js

---
+++ src/data/sampleData.js
@@ -0,0 +1,28 @@
+// Convert your Excel data to this format
+export const registrationData = [
+  { regNumber: "07-0001-01", callName: "BJ", handlerName: "Shirley Ottmer" },
+  { regNumber: "07-0001-02", callName: "Jaidon", handlerName: "Shirley Ottmer" },
+  { regNumber: "07-0001-03", callName: "Jake", handlerName: "Shirley Ottmer" },
+  { regNumber: "07-0001-04", callName: "Ozone", handlerName: "Shirley Ottmer" },
+  { regNumber: "07-0002-01", callName: "Liz", handlerName: "Russ Hornfisher" },
+  { regNumber: "07-0002-02", callName: "Morgan", handlerName: "Russ Hornfisher" },
+  { regNumber: "07-0003-01", callName: "Julie", handlerName: "Bonnie Hornfisher" },
+  { regNumber: "07-0003-02", callName: "Becky", handlerName: "Bonnie Hornfisher" },
+  { regNumber: "07-0003-03", callName: "Ben", handlerName: "Bonnie Hornfisher" },
+  // Add more entries from your Excel file...
+];
+
+export const classOptions = [
+  "Patrol 1", "Detective 2", "Investigator 3", "Super Sleuth 4", "Private Inv", 
+  "Det Diversions", "Ranger 1", "Ranger 2", "Ranger 3", "Ranger 4", "Ranger 5",
+  "Dasher 3", "Dasher 4", "Dasher 5", "Dasher 6", "Obedience 1", "Obedience 2",
+  "Obedience 3", "Obedience 4", "Obedience 5", "Starter", "Advanced", "Pro", "ARF",
+  "Zoom 1", "Zoom 1.5", "Zoom 2", "Games 1", "Games 2", "Games 3", "Games 4"
+];
+
+export const judgeOptions = [
+  "Linda Alberda", "Ginger Alpine", "Paige Alpine-Malone", "Anita Ambani", "Denise Ames",
+  "Youlia Anderson", "Cindy Angiulo", "Kalma Arnett", "Amy Atkinson", "Natasha Audy",
+  "Danica Auld", "Melanie Baker", "Cheramie Barbazuk", "Stephanie Barber", "Jeannine Barbour",
+  // Add more judges from your Excel file...
+];
